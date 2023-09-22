393b5ebc91de1e7df7d3f353ea98a0e82a71fbf2
Javi G Pisano <javiergpi@educastur.org>
2023-09-22 09:35:34 +0200

    UD1 Ejemplos Depurando JS

---
+++ ud1/ejemplos/1_depuracion_js/ejemplo3/js/ejemplo3.js
@@ -0,0 +1,28 @@
+/**
+ * DESIN- UD1 Ejemplo 1 -Uso del depurador JS. Encontrar los errores del script
+ * @author Javi G. Pisano
+ * 
+ * Realiza un script que se encargue de leer 10 nombres por pantalla y los inserte en un array. 
+ * Posteriormente, los nombres se mostrarán una unica ventana emergente
+ */
+
+/* Encontrar los errores que tiene el siguiente script, usando las herramientas de depuración de Google Chrome */
+
+var nombres=[];
+
+var NUMERO_NOMBRES=10;
+
+
+
+
+for (var i=0;i<numeroNombres-1;i++){
+	nombres[i]=promp("Introduce un nombre","Agapito");
+} 
+
+
+ for (var i=0;i<numeroNombres;i+1){
+	CADENA_NOMBRES=CADEN_ANOMBRES+"nombres[i]"+" ";
+} 
+ alert(nombres[i]);
+ 
+
